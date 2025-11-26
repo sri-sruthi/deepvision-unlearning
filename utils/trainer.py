e298e03b8f19289c25181e12f6e950b9c1b0c157
sri-sruthi <sruthimanikka@gmail.com>
2025-11-26 19:21:29 +0530

    Update trainer.py

---
+++ utils/trainer.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+import time
+
+def train_one_epoch(model, train_loader, criterion, optimizer, device):
+    model.train()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    for images, labels in tqdm(train_loader, desc="Training", leave=False):
+        images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item() * images.size(0)
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+
+    return running_loss / total, 100. * correct / total
+
+
+def validate(model, test_loader, criterion, device):
+    model.eval()
+    val_loss = 0.0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for images, labels in tqdm(test_loader, desc="Validating", leave=False):
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            val_loss += loss.item() * images.size(0)
+            _, predicted = outputs.max(1)
+            total += labels.size(0)
+            correct += predicted.eq(labels).sum().item()
+
+    return val_loss / total, 100. * correct / total
