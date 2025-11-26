eb1ae5e93f174a4ca121ec41b53f1daa9941b282
sri-sruthi <sruthimanikka@gmail.com>
2025-11-26 18:54:44 +0530

    Update transforms.py

---
+++ utils/transforms.py
@@ -0,0 +1,36 @@
+import torchvision.transforms as transforms
+
+def get_cifar10_transforms():
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.247, 0.243, 0.261))
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.247, 0.243, 0.261))
+    ])
+
+    return transform_train, transform_test
+
+
+def get_cifar100_transforms():
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5071, 0.4867, 0.4408),
+                             (0.2675, 0.2565, 0.2761))
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5071, 0.4867, 0.4408),
+                             (0.2675, 0.2565, 0.2761))
+    ])
+
+    return transform_train, transform_test
